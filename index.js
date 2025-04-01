0b13dfb9ebe9aabf0ead2204fecd757c8f813763
Abdalrahman-Mohammad <abd0004@hotmail.com>
2025-04-01 16:52:16 +0300

    Add function that create new task

--- index.js
+++ index.js
@@ -11,6 +11,45 @@
   }
 })();
 
+let addTaskBtn = document.getElementById("add-task");
+let newTaskElement = document.getElementById("new-task");
+
+addTaskBtn.addEventListener("click", () => {
+  console.log("clicked");
+
+  let newTaskValue = newTaskElement.value;
+
+  newTaskElement.value = "";
+
+  newTaskValue = String(newTaskValue).trim();
+  if (newTaskValue.length == 0) {
+    return;
+  }
+
+  if (new String(newTaskValue).indexOf("#") != -1) {
+    newTaskElement.value = newTaskValue;
+    alert("Please don't use #");
+    return;
+  }
+
+  console.log(newTaskValue);
+  console.log(localStorage.getItem("tasks"));
+
+  localStorage.setItem(
+    "tasks",
+    localStorage.getItem("tasks") +
+      (localStorage.getItem("tasks").length == 0 ? "" : "#") +
+      localStorage.getItem("id") +
+      "#" +
+      "Pinding"
+      +"#" +
+      newTaskValue
+  );
+  localStorage.setItem("id", parseInt(localStorage.getItem("id")) + 1);
+  console.log(localStorage.getItem("tasks"));
+  appendTask(parseInt(localStorage.getItem("id"))-1,newTaskValue);
+});
+
 function appendTask(id, status,task) {
   let tasks = document.getElementById("tasks");
   tasks.innerHTML += `
