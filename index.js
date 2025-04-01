9a8961d1440d762dbd3a6679eab53eb87bc71619
Abdalrahman-Mohammad <abd0004@hotmail.com>
2025-04-01 16:51:10 +0300

    Add appendTask function

--- index.js
+++ index.js
@@ -5,4 +5,22 @@
   if (localStorage.getItem("tasks") == null) {
     localStorage.setItem("tasks", "");
   }
+
 })();
+
+function appendTask(id, status,task) {
+  let tasks = document.getElementById("tasks");
+  tasks.innerHTML += `
+  <tr id="task" data-task-id="${id}">
+            <td id="id">${id}</td>
+            <td id="description">${task}</td>
+            <td id="status">${status}</td>
+            <td id="actions">
+              <button id="delete-btn">Delete</button>
+              <button id="edit-btn">Edit</button>
+              <button id="done-btn">Done</button>
+            </td>
+  </tr>
+            `;
+}
+// localStorage.clear();
